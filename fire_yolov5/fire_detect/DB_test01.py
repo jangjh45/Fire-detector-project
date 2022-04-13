56f4fff09ae3abd0efa6a6b33efcdbdd0a70bbbb
jangjh45 <jangjh45@naver.com>
2022-04-13 17:05:13 +0900

    Update DB_test01.py

--- fire_yolov5/fire_detect/DB_test01.py
+++ fire_yolov5/fire_detect/DB_test01.py
@@ -3,14 +3,16 @@ import glob
 import time
 import pymysql
 
+time.sleep(5)
+
 # 전역변수 선언부 
 db = None 
 cur = None
 
 conn = pymysql.connect(host='20.39.201.16',
                        user='root',
-                       password='0000', 
-                       charset='utf8', 
+                       password='0000',
+                       charset='utf8',
                        db='fire_detect') #DB 연결
                         
 cur = conn.cursor() #디폴트 커서 생성
@@ -22,21 +24,24 @@ txt_PATH = 'C:/yolov5-master/runs/detect/exp/labels/*.txt'
 fire_count = 0
 non_fire_count = 0
 
+no_txt_len = 0
+
 fire = "fire"
-non_fire = "non_fire"
 loading = "loading"
+non_fire = "non_fire"
+
 
 while(True):
     dir_list = os.listdir(dir_PATH)
     dir_count = len(dir_list)
     #print(dir_count)
-    if dir_count == 0: #폴더가 없으면 아래 코드 무시 1개이상 있으면 아래 코드 실행
+    if dir_count < 1: #폴더가 없으면 아래 코드 무시 1개이상 있으면 아래 코드 실행
         continue
     
     file_list = os.listdir(labels_PATH)
     file_count = len(file_list)
     #print(file_count)
-    if file_count < 2: #폴더안에 좌표값txt가 없으면 아래 코드 무시 1개이상 있으면 아래 코드 실행
+    if file_count < 1: #폴더안에 좌표값txt가 없으면 아래 코드 무시 1개이상 있으면 아래 코드 실행
         continue
     
     label_list = sorted(glob.glob(txt_PATH), key=os.path.getctime, reverse=True)
@@ -53,6 +58,8 @@ while(True):
         txt_len = len(a.readlines())
         a.close()
     
+    
+
     if first_list != second_list:   
         fire_count += 1 
         non_fire_count = 0
@@ -72,13 +79,13 @@ while(True):
 
     elif fire_count < 3 and non_fire_count < 3:
         sql = "INSERT INTO detect_table (state, detect_num, detect_time) VALUES (%s, %s, NOW());"
-        cur.execute(sql, (loading, txt_len))
+        cur.execute(sql, (loading, no_txt_len))
         print("상황파악중") 
 
     else:
         non_fire_count >= 3 and fire_count == 0
         sql = "INSERT INTO detect_table (state, detect_num, detect_time) VALUES (%s, %s, NOW());"
-        cur.execute(sql, (fire, txt_len))
+        cur.execute(sql, (non_fire, no_txt_len))
         print("화재 상황 종료")
 
     conn.commit()
