2e9879c7c63a2ceb2e093962bacadecae31bed0f
jangjh45 <jangjh45@naver.com>
2022-05-06 11:57:31 +0900

    Update app.py

--- fire_yolov5/fire_detect/app.py
+++ fire_yolov5/fire_detect/app.py
@@ -1,20 +1,19 @@
+import threading
 from flask import Flask, Response, render_template
-from flask_detect import detect
+from detect_and_db01 import fire_num, detect
 
-app = Flask(__name__)
+app2 = Flask(__name__)
 
-@app.route("/")
+@app2.route("/")
 def index():
     return render_template("index.html")
 
-
-
-@app.route('/video_feed')
+@app2.route('/video_feed')
 def video_feed():
     return Response(detect(),
         mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
-
 if __name__== "__main__":
-    app.run(debug = True)
+    thread1 = threading.Thread(target=fire_num)
+    thread1.start()
+    app2.run(debug=True)
