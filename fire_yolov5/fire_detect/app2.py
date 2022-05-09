5f3c8a6ab2a1d408d764429284a0e3abf67e965d
jangjh45 <jangjh45@naver.com>
2022-05-09 17:40:00 +0900

    0509up

--- fire_yolov5/fire_detect/app2.py
+++ fire_yolov5/fire_detect/app2.py
@@ -1,17 +1,30 @@
 import threading
 from flask import Flask, Response, render_template
 from detect_and_db01 import detect, fire_num
+import pymysql
 
 app2 = Flask(__name__)
 
 @app2.route("/")
 def index():
-    return render_template("index2.html")
+    db = pymysql.connect(host='20.194.30.39',
+                         user='fire',
+                         password='0000',
+                         charset='utf8',
+                         db='fire_detect')
+    cursor = db.cursor()
+    sql = "SELECT * FROM temper ORDER BY time DESC limit 1;"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for record in result:
+        return render_template("index2.html", T=record[1], H=record[2])
 
-@app2.route('/video_feed')
+
+@app2.route("/video_feed")
 def video_feed():
     return Response(detect(),
         mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__== "__main__":
     thread1 = threading.Thread(target=fire_num)
